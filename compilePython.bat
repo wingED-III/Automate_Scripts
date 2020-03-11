fd1a902a05d657dcc70b06771407993def40136c
WingWM <60010106@kmitl.ac.th>
2020-03-12 03:56:50 +0700

    Update compilePython.bat
    Done
    -complie and move to working directory

--- compilePython.bat
+++ compilePython.bat
@@ -1,4 +1,13 @@
 @echo off
 set /p pyFile= "Enter python file name that you want to compile: "
-pyinstaller.exe -F %pyFile%
+::pyinstaller.exe -F %pyFile% 
+set pyFile=%pyFile:~0,-4%
+echo %pyFile%
+set x=.exe"
+echo %x%
+set pyFile=%pyFile%%x%
+echo %pyFile%
+
+echo %cd%
+move %cd%\dist\%pyFile%  %cd%
 pause
