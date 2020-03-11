2768089c727e60fc5db8b1d00866afc4b9a65d10
WingWM <60010106@kmitl.ac.th>
2020-03-12 03:58:19 +0700

    Update compilePython.bat
    fomat

--- compilePython.bat
+++ compilePython.bat
@@ -1,13 +1,17 @@
 @echo off
 set /p pyFile= "Enter python file name that you want to compile: "
-::pyinstaller.exe -F %pyFile% 
+pyinstaller.exe -F %pyFile% 
 set pyFile=%pyFile:~0,-4%
-echo %pyFile%
-set x=.exe"
-echo %x%
-set pyFile=%pyFile%%x%
-echo %pyFile%
 
-echo %cd%
+REM echo %pyFile%
+
+set x=.exe"
+
+REM echo %x%
+set pyFile=%pyFile%%x%
+
+REM echo %pyFile%
+REM echo %cd%
+
 move %cd%\dist\%pyFile%  %cd%
 pause
