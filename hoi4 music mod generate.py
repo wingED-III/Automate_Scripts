49e05ec9e851771fb1c65e584b284e06be08390a
WingWM <60010106@kmitl.ac.th>
2020-03-12 02:24:11 +0700

    Create hoi4 music mod generate.py

---
+++ hoi4 music mod generate.py
@@ -0,0 +1,36 @@
+import os
+
+def createMusicTXT(musicList,playlistName):
+    musicTxt = open("test.txt","w+")
+
+    musicTxt.write("music_station = "+playlistName+"\n\n")
+    musicList = map(lambda fileX:code_for_a_song(fileX) ,musicList)
+    musicTxt.writelines(musicList)
+    musicTxt.close()
+
+def formatFilename(fileName):
+    charToReplace = "- +"
+    for i in range(len(fileName)):
+        if fileName[i] in charToReplace:
+            fileName = fileName.replace(fileName[i],"_")
+            #print(fileName[i]+":",fileName)
+    fileName = fileName[:-4].lower()+"\n" # exclude extension 
+    return fileName 
+
+def code_for_a_song(fileName):
+    fileName = formatFilename(fileName)
+    return "music ={\n\t"+fileName+"\n}\n\n" 
+    
+#create str below    
+#music = {
+# 	song = "songName"
+#}   
+
+
+extensionTarget = ".ogg"
+items = os.listdir()
+#print(items)
+musicOgglist = filter(lambda fileX:extensionTarget == os.path.splitext(fileX)[1]  ,items) # os.path.splitext(fileX)[1] # 0 is a file name.
+musicOgglist = list(musicOgglist)
+playlistName = input("Enter playlist name:")
+createMusicTXT(musicOgglist,playlistName)
