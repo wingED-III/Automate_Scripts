05c2a3658ec4ea3583af767e46d17b414cea950b
WingWM <60010106@kmitl.ac.th>
2020-03-16 20:10:01 +0700

    Update readFile.py

--- readFile.py
+++ readFile.py
@@ -1,8 +1,30 @@
 import os
+from pathlib import Path
 
 fileName = "Touhou 6-17 characters details.txt"
 filePath = "myResource/"+fileName
 script_directory = os.path.dirname(__file__)
 filePath = os.path.join(script_directory,filePath)
 
-file1 = open(filePath,'r')
+if Path(filePath).is_file():
+    print ("File exist")
+else:
+    print ("File not exist")
+
+
+file1 = open(filePath,'r',encoding="utf-8")
+
+data = file1.read()
+data = data.split("\n")
+
+print(data)
+# for line in data:
+#     if line
+#     print(line)
+
+
+
+
+
+
+file1.close()
