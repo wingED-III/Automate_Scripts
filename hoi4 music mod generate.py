69f4e8e7769a61f2bc5deb8da6a2547addba1a70
WingWM <60010106@kmitl.ac.th>
2020-03-16 17:27:05 +0700

    little format

--- hoi4 music mod generate.py
+++ hoi4 music mod generate.py
@@ -3,9 +3,9 @@ import os
 def createMusicTXT(musicList,playlistName):
     musicTxt = open(playlistName+"_song.txt","w+")
 
-    musicTxt.write("music_station = "+playlistName+"\n\n")
-    musicList = map(lambda fileX:code_for_a_song(fileX) ,musicList)
-    musicTxt.writelines(musicList)
+    musicTxt.write("music_station = "+playlistName+"\n\n")          #write head
+    musicList = map(lambda fileX:code_for_a_song(fileX) ,musicList) #generate Code from song Name
+    musicTxt.writelines(musicList)                                  #write str list to file
     musicTxt.close()
 
 def formatFilename(fileName):
@@ -18,18 +18,20 @@ def formatFilename(fileName):
     return fileName 
 
 def code_for_a_song(fileName):
+    #create str below    
+    #music = {
+    # 	song = "songName"
+    #}   
     fileName = formatFilename(fileName)
     return "music ={\n\t"+fileName+"\n}\n\n" 
-    
-#create str below    
-#music = {
-# 	song = "songName"
-#}   
+
 
 
 extensionTarget = ".ogg"
 items = os.listdir()
 #print(items)
+
+#check extension
 musicOgglist = filter(lambda fileX:extensionTarget == os.path.splitext(fileX)[1]  ,items) # os.path.splitext(fileX)[1] # 0 is a file name.
 musicOgglist = list(musicOgglist)
 playlistName = input("Enter playlist name [No spacebar,only english lowercase alphabet]:\n")
