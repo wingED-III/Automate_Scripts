bc22297b3f589c6ed79e7fd12a91bcfff2551f94
WingWM <60010106@kmitl.ac.th>
2020-03-16 17:42:17 +0700

    Update getCountrytags.py
    extrac tags successful.

--- getCountrytags.py
+++ getCountrytags.py
@@ -1,22 +1,25 @@
 import os
+import glob
 
 def strList2File(strList,fileName):
     fileTxt = open(fileName+".txt","w+")
     fileTxt.writelines(strList)
     fileTxt.close()
 
+
+outputFile = "hoi4_all_country_tags"
+pathContainFiles = "\myResource\countries"
 thisFile = __file__.replace(".","",1) #remove . at the front of str
 fullPath = os.path.realpath(__file__) # reutrn  fullpath+filename
 CurrentPath = fullPath.replace(thisFile,"") #get current path
 #print(thisFile)
 #print(CurrentPath)
+targetPath = CurrentPath+pathContainFiles
 
-items = os.listdir(os.path.join(os.path.dirname(__file__),"/myResource/countries.txt"))
-print(items)
-# #for i in items:
-# #    i = i.replace(" - ",".")
-#     #print(i.split(".")[0])
-# items = list(map(lambda i:i.replace(" - ",".").split(".")[0] ,items))
-# #print(items)
-# for i in range(len(items)):
-#         print(i,items[i])
+items = os.listdir(targetPath)
+#print(items)
+items = list(map(lambda i:i.replace(" - ",".").split(".")[0]+"\n" ,items))
+#print(items)
+
+strList2File(items,outputFile)
+
