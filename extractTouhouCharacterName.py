57d3c6706abd259d5f02b3035f9e6fc82faf7721
WingWM <60010106@kmitl.ac.th>
2020-03-16 21:37:28 +0700

    Update extractTouhouCharacterName.py
    -format

--- extractTouhouCharacterName.py
+++ extractTouhouCharacterName.py
@@ -3,48 +3,55 @@ from pathlib import Path
 import pandas as pd
 
 
+def main():
+    fileName = "Touhou 6-17 characters details.txt"
+    outputFile = "Touhou first Name Only"
 
-fileName = "Touhou 6-17 characters details.txt"
-outputFile = "Touhou characterName"
+    filePath = "myResource/"+fileName
+    script_directory = os.path.dirname(__file__)
+    filePath = os.path.join(script_directory,filePath)
 
-filePath = "myResource/"+fileName
-script_directory = os.path.dirname(__file__)
-filePath = os.path.join(script_directory,filePath)
-
-if Path(filePath).is_file():
-    print ("File exist")
-else:
-    print ("File not exist")
+    if Path(filePath).is_file():
+        print ("File exist")
+    else:
+        print ("File not exist")
+        return
 
 
 
-file1 = open(filePath,'r',encoding="utf-8")
+    file1 = open(filePath,'r',encoding="utf-8")
 
-data = file1.read()
+    data = file1.read()
 
-file1.close()
+    file1.close()
 
-data = data.split("\n")
+    data = data.split("\n")
 
-#print(data)
-prev = data[0]
-i = 0
-characterNameList = []
+    #print(data)
+    prev = data[0]
+    i = 0
+    characterNameList = []
 
-for line in data:
+    for line in data:
 
-    if "Species" in line:
-        i+=1
-        nameLine = prev.split()
-        firstname,surname = nameLine[0],nameLine[1]
-        fullName = firstname+" "+surname
-        characterNameList.append(fullName)
-        #print("{}  {}".format(i,fullName))
+        if "Species" in line:
+            i+=1
+            nameLine = prev.split()
+            firstname,surname = nameLine[0],nameLine[1]
+            fullName = firstname+" "+surname
+            #characterNameList.append(fullName)
+            #print("{}  {}".format(i,fullName))
 
-    prev = line
+            characterNameList.append(""+firstname)
+            
+        prev = line
 
-#print(characterNameList)
+    #print(characterNameList)
 
-df = pd.DataFrame(characterNameList)
-#print(df)
-df.to_csv(outputFile+'.csv')
+    # Write to csv
+    df = pd.DataFrame(characterNameList)
+    #print(df)
+    df.to_csv(outputFile+'.csv')
+
+if __name__ == "__main__":
+    main()
