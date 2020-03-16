a085b68416d16a4a3965dd6592f3f9f81aa8fce7
WingWM <60010106@kmitl.ac.th>
2020-03-16 20:44:23 +0700

    Update readFile.py
    -extract name Success 80%
    - after this process by human

--- readFile.py
+++ readFile.py
@@ -1,7 +1,12 @@
 import os
 from pathlib import Path
+import pandas as pd
+
+
 
 fileName = "Touhou 6-17 characters details.txt"
+outputFile = "Touhou characterName"
+
 filePath = "myResource/"+fileName
 script_directory = os.path.dirname(__file__)
 filePath = os.path.join(script_directory,filePath)
@@ -12,19 +17,34 @@ else:
     print ("File not exist")
 
 
+
 file1 = open(filePath,'r',encoding="utf-8")
 
 data = file1.read()
-data = data.split("\n")
-
-print(data)
-# for line in data:
-#     if line
-#     print(line)
-
-
-
-
-
 
 file1.close()
+
+data = data.split("\n")
+
+#print(data)
+prev = data[0]
+i = 0
+characterNameList = []
+
+for line in data:
+
+    if "Species" in line:
+        i+=1
+        nameLine = prev.split()
+        firstname,surname = nameLine[0],nameLine[1]
+        fullName = firstname+" "+surname
+        characterNameList.append(fullName)
+        #print("{}  {}".format(i,fullName))
+
+    prev = line
+
+#print(characterNameList)
+
+df = pd.DataFrame(characterNameList)
+#print(df)
+df.to_csv(outputFile+'.csv')
